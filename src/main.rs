b8ee27e1690a8efe45d08d11bd54a7996ecc27ed
Fern <Nope>
2026-05-30 20:38:41 -0400

    started on main prototyping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 struct Item {
     letter: char,
     //indicates what words might be "on" the letter
@@ -9,12 +10,43 @@ struct Item {
 fn main() {
     let args: Vec<String> = env::args().collect();
     // cargo run -- (word file) (target file) (special args)
+    let words: Vec<String> = read_input(args[1].clone());
 
-
+    let width = 5;
+    let board = assemble_board(width, words, false);
+    for i in 0..width { 
+        let mut s = String::new();
+        for j in 0..width{
+            s.push(board[i][j].letter);
+        }
+        println!("{s}");
+    }
 }
 
-fn assemble_board(width: usize, words: Vec<String>, flag: bool) -> Vec<Vec<Item>>{
-    let width = 3;
+fn read_input(s: String) -> Vec<String>{
+    let mut words: Vec<String> = Vec::new();
+    let s = s + ".txt";
+    let contents = fs::read_to_string(s).expect("Couldn't read file");
+    let contents: &[u8] = contents.as_bytes(); 
+    let mut temp: Vec<u8> = Vec::new();    
+    let mut i = 0;
+    while i < contents.len() - 1{
+        let snippet: u8 = contents[i];
+        if snippet.is_ascii_alphabetic(){
+            temp.push(contents[i]);
+        }
+        if contents[i] == 92 && contents[i + 1] == 110 {                                      
+            words.push(String::from_utf8(temp).expect("Found invalid UTF8"));
+            temp = Vec::new();
+            i += 2;
+        }
+        i += 1;
+    }
+    println!("{:?}", words);
+    words
+}
+
+fn assemble_board(width: usize, words: Vec<String>, transparent: bool) -> Vec<Vec<Item>>{
     let mut board: Vec<Vec<Item>> = Vec::new();
     for i in 0..width {
         let mut row: Vec<Item> = Vec::new();
@@ -27,11 +59,5 @@ fn assemble_board(width: usize, words: Vec<String>, flag: bool) -> Vec<Vec<Item>
         }
         board.push(row);
     }
-
-    for i in 0..width { 
-        let s = format!("{}{}{}", board[i][0].letter, board[i][1].letter, board[i][2].letter);
-        println!("{s}");
-    }
-
     board
 }
